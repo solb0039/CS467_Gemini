ab4bdf50accfcf2382afac3dfbb6d22dc45efb1b
LK <16886594+VZee@users.noreply.github.com>
2018-07-21 09:40:29 -0500

    Sample awards table

---
+++ db/sampleAwards.sql
@@ -0,0 +1,5 @@
+-- Insert data into table `awards`
+INSERT INTO `awards` (`award_id`, `type`, `first_name`, `last_name`, `email`, `time`, `date`, `user_id`) VALUES
+(1, 'Employee of the Month', 'John', 'English', 'jenglish@email.com', '09:00:00', '2018-09-01', '1'),
+(2, 'Employee of the Month', 'Emily', 'Doe', 'edoe@email.com', '10:15:00', '2018-10-01', '1'),
+(3, 'Employee of the Quarter', 'Beth', 'Jane', 'bjane@email.com', '14:12:00', '2018-09-01', '2');
